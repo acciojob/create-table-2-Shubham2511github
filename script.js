381afc97b667b0815c8cab2969c5ce5ddf53b396
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-28 11:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
     //Write your code here
+	var rowCount = prompt("Input number of rows");
+	var columnsCount = prompt("Input number of columns");
+
+	var table = document.getElementById("myTable");
+	table.innerHTML = '';
+	for(let i =0; i<rowCount; i++){
+		var row = document.createElement('tr');
+		for(let j =0; j<columnsCount; j++){
+			var cell = document.createElement('td');
+			cell.textContent = `Row-${i} Column-${j}`;
+			row.appendChild(cell);
+		}
+		table.appendChild(row);
+	}
   
 }
